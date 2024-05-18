4eeed6eb991593aa8f28536b6da6d3d19f0f6aab
reazul7 <islamreazul7777@gmail.com>
2024-05-18 19:15:04 +0600

    added navbar and footer

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 ## Project Run command
+
 yarn run dev
 
 ## Format using Prettier command
+
 yarn format
 
-## Technologies 
--- React
--- Tailwind CSS
--- DaisyUI
--- ESLint, Prettier
+## Technologies
+
+-   React
+-   Tailwind CSS
+-   DaisyUI
+-   ESLint, Prettier
---
+++ src/Layout/Main.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import Footer from '../pages/Home/Home/Shared/Footer/Footer';
+import NavBar from '../pages/Home/Home/Shared/NavBar/NavBar';
+
+export default function Main() {
+    return (
+        <div>
+            <NavBar />
+            <Outlet />
+            <Footer />
+        </div>
+    );
+}
