cb9da529ba3dbfa769b7459546e986c7729b9a08
reazul7 <islamreazul7777@gmail.com>
2024-05-18 21:33:08 +0600

    create sectionTitle component

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -8,10 +8,12 @@ import slide2 from "../../../assets/home/slide2.jpg";
 import slide3 from "../../../assets/home/slide3.jpg";
 import slide4 from "../../../assets/home/slide4.jpg";
 import slide5 from "../../../assets/home/slide5.jpg";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 export default function Category() {
     return (
-        <div className="my-16">
+        <section className="my-16">
+            <SectionTitle subHeading={"From 11.00am to 10.00pm"} heading={"Order Online"}/>
             <Swiper
                 slidesPerView={1}
                 spaceBetween={10}
@@ -56,6 +58,6 @@ export default function Category() {
                     <h3 className="text-4xl uppercase text-center -mt-20 text-white">Salads</h3>
                 </SwiperSlide>
             </Swiper>
-        </div>
+        </section>
     );
 }
