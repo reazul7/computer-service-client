72ef62d70938c99ea9f8cc42d26c71f15ac43460
reazul7 <islamreazul7777@gmail.com>
2024-05-18 22:43:56 +0600

    Added Featured component

---
+++ src/pages/Home/Featured/Featured.jsx
@@ -0,0 +1,25 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import featuredImage from "../../../assets/home/featured.jpg";
+import "./Featured.css";
+
+export default function Featured() {
+    return (
+        <div className="featured-items bg-fixed text-white pt-8 my-20">
+            <SectionTitle subHeading={"Check it out"} heading={"Featured Item"} />
+            <div className="md:flex justify-center items-center bg-slate-500 bg-opacity-40 pb-20 pt-12 px-36">
+                <div>
+                    <img src={featuredImage} alt="" />
+                </div>
+                <div className="md:ml-10">
+                    <p>Aug 20, 2029</p>
+                    <p className="uppercase">Where can i get some?</p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur aut temporibus deleniti iusto facilis deserunt
+                        laudantium ea nobis assumenda vitae.
+                    </p>
+                    <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -6,21 +6,21 @@ import MenuItem from "../Home/Shared/MenuItem/MenuItem";
 export default function PopularMenu() {
     const [menu, setMenu] = useState([]);
     useEffect(() => {
-        fetch('menu.json')
-        .then(res => res.json())
-        .then(data => {
-            const PopularItems = data.filter(item => item?.category === 'popular');
-            setMenu(PopularItems);
-        })
+        fetch("menu.json")
+            .then(res => res.json())
+            .then(data => {
+                const PopularItems = data.filter(item => item?.category === "popular");
+                setMenu(PopularItems);
+            });
     }, []);
-  return (
-    <section className="mb-12">
-        <SectionTitle subHeading={"Popular Items"} heading={"From Our Menu"}/>
-        <div className="grid md:grid-cols-2 gap-10">
-            {
-                menu?.map((item) => <MenuItem key={item?._id} item={item}></MenuItem>)
-            }
-        </div>
-    </section>
-  )
+    return (
+        <section className="mb-12">
+            <SectionTitle subHeading={"Popular Items"} heading={"From Our Menu"} />
+            <div className="grid md:grid-cols-2 gap-10">
+                {menu?.map(item => (
+                    <MenuItem key={item?._id} item={item}></MenuItem>
+                ))}
+            </div>
+        </section>
+    );
 }
