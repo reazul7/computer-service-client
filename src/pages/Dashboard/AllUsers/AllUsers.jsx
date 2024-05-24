ecaecd0aa4c97f938629dd2722d8e104d05f7d43
reazul7 <islamreazul7777@gmail.com>
2024-05-24 16:43:04 +0600

    Make Admin and Delete User

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,15 +1,61 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaTrashAlt, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 export default function AllUsers() {
     const axiosSecure = useAxiosSecure();
-    const { data: users = [] } = useQuery({
+
+    const { data: users = [], refetch } = useQuery({
         queryKey: ["users"],
         queryFn: async () => {
             const res = await axiosSecure.get("/users");
             return res.data;
         },
     });
+
+    const handleMakeAdmin = user => {
+        // /users/admin/:id
+        axiosSecure.patch(`/users/admin/${user._id}`).then(res => {
+            if (res.data.modifiedCount > 0) {
+                refetch();
+                Swal.fire({
+                    icon: "success",
+                    title: `${user.name} is now an admin`,
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+            }
+        });
+    };
+
+    const handleDeleteUser = user => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!",
+        }).then(result => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/users/${user._id}`).then(res => {
+                    console.log("delete", res.data);
+                    if (res.data.deletedCount > 0) {
+                        refetch();
+                        Swal.fire({
+                            icon: "success",
+                            title: "User deleted successfully",
+                            showConfirmButton: false,
+                            timer: 1500,
+                        });
+                    }
+                });
+            }
+        });
+    };
+
     return (
         <div className="py-5">
             <div className="flex justify-evenly">
@@ -31,7 +77,7 @@ export default function AllUsers() {
                     <tbody>
                         {users?.map((user, index) => (
                             <tr key={user?._id}>
-                                <th>{index + 1}</th>
+                                <td>{index + 1}</td>
                                 <td>
                                     <div className="flex items-center gap-3">
                                         <div className="avatar">
@@ -43,11 +89,20 @@ export default function AllUsers() {
                                 </td>
                                 <td>{user?.name}</td>
                                 <td>{user?.email}</td>
-                                {/* <th>
-                                    <button onClick={() => handleDelete(user?._id)} className="btn btn-ghost btn-md">
-                                        <FaTrashAlt className="text-red-600" />{" "}
+                                <td>
+                                    {user?.role === "admin" ? (
+                                        "Admin"
+                                    ) : (
+                                        <button onClick={() => handleMakeAdmin(user)} className="btn bg-orange-500 btn-md">
+                                            <FaUsers className="text-white text-2xl" />{" "}
+                                        </button>
+                                    )}
+                                </td>
+                                <td>
+                                    <button onClick={() => handleDeleteUser(user)} className="btn btn-ghost btn-md">
+                                        <FaTrashAlt className="text-red-600 text-2xl" />{" "}
                                     </button>
-                                </th> */}
+                                </td>
                             </tr>
                         ))}
                     </tbody>
