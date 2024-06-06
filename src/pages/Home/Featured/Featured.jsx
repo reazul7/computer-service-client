dc01e5574e0406d4c40b734a5d39389a15724f3f
reazul7 <islamreazul7777@gmail.com>
2024-06-07 02:03:54 +0600

    Featured  Image upload

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -1,11 +1,11 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
-import featuredImage from "../../../assets/home/featured.jpg";
+import featuredImage from "../../../assets/service/desktop-repair.jpg";
 import "./Featured.css";
 
 export default function Featured() {
     return (
         <div className="featured-items bg-fixed text-white pt-8 my-20">
-            <SectionTitle subHeading={"Check it out"} heading={"Featured Item"} />
+            <SectionTitle subHeading={"Check it out"} heading={"Featured Service"} />
             <div className="md:flex justify-center items-center bg-slate-500 bg-opacity-40 pb-20 pt-12 px-36">
                 <div>
                     <img src={featuredImage} alt="" />
