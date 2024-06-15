e086a2fed68dfb2e5e5f76d06b0dbb2a44e6c12e
reazul7 <islamreazul7777@gmail.com>
2024-06-15 23:28:21 +0600

    User Home page update

--- src/pages/Dashboard/UserHome/UserHome.jsx
+++ src/pages/Dashboard/UserHome/UserHome.jsx
@@ -1,11 +1,47 @@
-import useAuth from "../../../hooks/useAuth";
+import { useContext } from "react";
+import { Helmet } from "react-helmet-async";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 export default function UserHome() {
-    const { user } = useAuth();
+    const { user } = useContext(AuthContext);
+    const userProfilesData = [
+        { name: "Email", value: `${user?.email}` },
+        { name: "Email Verified", value: `${user?.emailVerified === false ? "No" : "Yes"}` },
+        { name: "Phone Number", value: `${user?.phoneNumber === null ? "Not Added" : "Not Added"}` },
+    ];
     return (
         <div>
-            <h2 className="text-3xl">Hi, Welcome</h2>
-            <span>{user?.displayName ? user?.displayName : "Black"}</span>
+            <Helmet>
+                <title>Profile | Computer Service</title>
+            </Helmet>
+            <div>
+                <div className="hero min-h-screen bg-base-200">
+                    <div className="hero-content text-center">
+                        <div className="max-w-md">
+                            <div className="avatar online">
+                                <div className="w-32 rounded-full">
+                                    <img src={user?.photoURL} alt={user?.displayName} />
+                                </div>
+                            </div>
+
+                            <h1 className="text-5xl font-bold">{user?.displayName}</h1>
+                            <div className="overflow-x-auto py-5">
+                                <table className="table">
+                                    <tbody>
+                                        {userProfilesData?.map((userProfileData, index) => (
+                                            <tr key={index}>
+                                                <th className="p-1">{userProfileData?.name}</th>
+                                                <td className="p-1">:</td>
+                                                <td className="p-1 px-2">{userProfileData?.value}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
